053133132996a05e9b71198877a2b7698d70e8cb
Yazon Suleiman <yazonsuleiman@yazons-mbp.attlocal.net>
2019-09-30 00:02:00 -0700

    test

---
+++ app.js
@@ -0,0 +1,34 @@
+const org1_depts = [
+    {
+      name: 'accounting',
+      children: [
+        { name: 'accounting payable', children: [] },
+        { name: 'accounting receivable', children: [] },
+      ],
+    },
+    {
+      name: 'finance',
+      children: [],
+    },
+  ]
+  
+  const org2_depts = [
+    {
+      name: 'accounting',
+      children: [
+        { name: 'accounting payable', children: [] },
+        {
+          name: 'accounting receivable',
+          children: [{ name: 'cash', children: [] }, { name: 'check', children: [] }],
+        },
+      ],
+    },
+    {
+      name: 'finance',
+      children: [{ name: 'investment', children: [] }],
+    },
+  ]
+  
+
+
+  
