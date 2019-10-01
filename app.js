a1f8a61cd5a86114c91fce62043b95a04e70bc5a
Yazon Suleiman <yazonsuleiman@yazons-mbp.attlocal.net>
2019-09-30 17:55:36 -0700

    test

--- app.js
+++ app.js
@@ -1,34 +1,68 @@
+// DATA
+
 const org1_depts = [
-    {
-      name: 'accounting',
-      children: [
-        { name: 'accounting payable', children: [] },
-        { name: 'accounting receivable', children: [] },
-      ],
-    },
-    {
-      name: 'finance',
-      children: [],
-    },
-  ]
-  
-  const org2_depts = [
-    {
-      name: 'accounting',
-      children: [
-        { name: 'accounting payable', children: [] },
-        {
-          name: 'accounting receivable',
-          children: [{ name: 'cash', children: [] }, { name: 'check', children: [] }],
-        },
-      ],
-    },
-    {
-      name: 'finance',
-      children: [{ name: 'investment', children: [] }],
-    },
-  ]
-  
+  {
+    name: 'accounting',
+    children: [
+      { name: 'accounting payable', children: [] },
+      { name: 'accounting receivable', children: [] },
+    ],
+  },
+  {
+    name: 'finance',
+    children: [],
+  },
+]
+
+const org2_depts = [
+  {
+    name: 'accounting',
+    children: [
+      { name: 'accounting payable', children: [] },
+      {
+        name: 'accounting receivable',
+        children: [{ name: 'cash', children: [] }, { name: 'check', children: [] }],
+      },
+    ],
+  },
+  {
+    name: 'finance',
+    children: [{ name: 'investment', children: [] }],
+  },
+]
+
+// FUNCTION
+
+function listOrg(orgData,targetUL) {
+  var i;
+  for (i = 0; i < orgData.length; i++) {
+    if (orgData[i].hasOwnProperty('name')) {
+      // Take out the text from the .name property
+      var nameText = document.createTextNode(orgData[i].name);
+      // Define a new <li> tag
+      var newLI = document.createElement('li');
+      // Append text to new <li> tage - newLI
+      newLI.appendChild(nameText);
+      // Append element to <ul> tag
+      targetUL.appendChild(newLI);
+    }
+
+    if (orgData[i].hasOwnProperty('children')) {
+      // Define new <ul> tag
+      var newUL = document.createElement('ul');
+      // Append new <ul> tag
+      var lowUl = targetUL.appendChild(newUL);
+      // Select new lower level <ul> tag
+      var lowUL = targetUL.querySelector('ul');
+      // Recurse
+      listOrg(orgData[i].children,lowUL );
+    }
+  }
+}
+
+// CALL FUNCTION
+PrintDepts(org1_depts,document.getElementById("org1"));
+PrintDepts(org2_depts,document.getElementById("org2"));
 
 
   
